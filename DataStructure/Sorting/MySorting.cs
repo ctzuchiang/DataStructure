f5bb1d9ac9c6807f4dcd2577258ed93a78be2c40
joseph.chao <joseph.chao@titansoft.com.sg>
2017-06-22 15:53:12 +0800

    update

--- DataStructure/Sorting/MySorting.cs
+++ DataStructure/Sorting/MySorting.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataStructure.Sorting
+﻿namespace DataStructure.Sorting
 {
-    class MySorting
+    public class MySorting
     {
+        public int[] Sorting(int[] s)
+        {
+            var tmp = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < s.Length - 1; j++)
+                {
+                    if (s[j] > s[j + 1])
+                    {
+                        tmp = s[j];
+                        s[j] = s[j + 1];
+                        s[j + 1] = tmp;
+                    }
+                }
+            }
+
+            return s;
+        }
     }
 }
---
+++ DataStructureTest/Sorting/MySortingTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Linq;
+using DataStructure.Sorting;
+using ExpectedObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructureTest.Sorting
+{
+    [TestClass]
+    public class MySortingTest
+    {
+        private int[] _Data;
+        private const int _DataQuantity = 1000;
+        
+        [TestInitialize]
+        public void SetUp()
+        {
+            Random rnd = new Random();
+
+            _Data = new int[_DataQuantity];
+            for (int i = 0; i < _DataQuantity; i++)
+            {
+                _Data[i] = rnd.Next(0, 5000);
+            }
+        }
+
+        [TestMethod]
+        public void SortingTest()
+        {
+            var expected = _Data.OrderBy(i => i).ToArray();
+
+            Stopwatch watch = new Stopwatch();
+
+            watch.Start();
+            var actual = new MySorting().Sorting(_Data);
+            watch.Stop();
+
+            Console.WriteLine(watch.Elapsed);
+
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+    }
+}
