8cc0f6099c804e68a4653056f81ad3c4f294f844
joseph.chao <joseph.chao@titansoft.com.sg>
2017-06-22 17:28:33 +0800

    update

---
+++ DataStructure/InterviewQuestion/StarTrees.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DataStructure.InterviewQuestion
+{
+    public class StarTrees
+    {
+        public void StarTree_1(int level)
+        {
+            for (int i = 1; i <= level; i++)
+            {
+                for (int j = 1; j <= level - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j < i*2; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void StarTree_2(int level)
+        {
+            for (int i = 0; i < level; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 1; j < (level-i)*2; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ DataStructureTest/InterviewQuestion/StarTreesTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DataStructure.InterviewQuestion;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructureTest.InterviewQuestion
+{
+    [TestClass]
+    public class StarTreesTest
+    {
+        [TestMethod]
+        public void StarTree_Test()
+        {
+            Console.WriteLine("StarTree_1\n");
+            new StarTrees().StarTree_1(5);
+            Console.WriteLine("\nStarTree_2\n");
+            new StarTrees().StarTree_2(5);
+        }
+    }
+}
