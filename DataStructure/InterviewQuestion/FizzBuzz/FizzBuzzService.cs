00bec8b47623ea72c2691d67fd435c5482696413
joseph.chao <joseph.chao@titansoft.com.sg>
2017-07-01 16:07:21 +0800

    update

---
+++ DataStructure/InterviewQuestion/FizzBuzz/FizzBuzzService.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructure.InterviewQuestion.FizzBuzz
+{
+    public class FizzBuzzService
+    {
+        public IEnumerable<object> FizzBuzz_string(int loopRange, int[] nArray)
+        {
+            string result;
+            var resultArray = new object[loopRange];
+
+            for (int i = 1; i <= loopRange; i++)
+            {
+                result = "";
+
+                foreach (var n in nArray)
+                {
+                    if (i % n == 0)
+                        result += n;
+                }
+
+                if (result.Length == 0)
+                    resultArray[i - 1] = i;
+                else
+                {
+                    resultArray[i - 1] = "\"" + result + "\"";
+                }
+            }
+            return resultArray;
+        }
+
+        public IEnumerable<object> FizzBuzz_stringBuilder(int loopRange, int[] nArray)
+        {
+            StringBuilder result = new StringBuilder();
+            var resultArray = new object[loopRange];
+
+            for (int i = 1; i <= loopRange; i++)
+            {
+                result.Clear();
+
+                foreach (var n in nArray)
+                {
+                    if (i % n == 0)
+                        result.Append(n);
+                }
+
+                if (result.Length == 0)
+                    resultArray[i - 1] = i;
+                else
+                {
+                    resultArray[i - 1] = "\"" + result + "\"";
+                }
+            }
+            return resultArray;
+        }
+
+    }
+}
--- DataStructureTest/InterviewQuestion/FizzBuzz/FizzBuzzTest.cs
+++ DataStructureTest/InterviewQuestion/FizzBuzz/FizzBuzzTest.cs
@@ -1,53 +1,77 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using DataStructure.InterviewQuestion.FizzBuzz;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExpectedObjects;
 
 namespace DataStructureTest.InterviewQuestion.FizzBuzz
 {
     [TestClass]
     public class FizzBuzzTest
     {
-        private List<string> _ExpectedList;
-        private FizzBuzzC _FizzBuzzC;
+        private IEnumerable<object> _ExpectedList;
+        private FizzBuzzService _FizzBuzzService;
 
+        [TestInitialize]
         public void SetUp()
         {
-            _FizzBuzzC = new FizzBuzzC();
-            _ExpectedList = new List<string>
+            _FizzBuzzService = new FizzBuzzService();
+            _ExpectedList = new object[]
             {
-                "1",
-                "2",
-                "Fizz",
-                "4",
-                "Buzz",
-                "Fizz",
-                "7",
-                "8",
-                "Fizz",
-                "Buzz",
-                "11",
-                "Fizz",
-                "13",
-                "14",
-                "FizzBuzz"
+                1,
+                2,
+                "\"3\"",
+                4,
+                "\"5\"",
+                "\"3\"",
+                "\"7\"",
+                8,
+                "\"3\"",
+                "\"5\"",
+                11,
+                "\"3\"",
+                13,
+                "\"7\"",
+                "\"35\"",
+                16,
+                17,
+                "\"3\"",
+                19,
+                "\"5\"",
+                "\"37\"",
             };
         }
 
         [TestMethod]
-        public void FizzBuzz_3_Test()
+        public void FizzBuzz_string_Test()
         {
-            SetUp();
-            Assert.AreEqual(_FizzBuzzC.FizzBuzz_3(15), _ExpectedList.ToString());
+            var actual = _FizzBuzzService.FizzBuzz_string(21, new[] { 3, 5, 7 });
+            _ExpectedList.ToExpectedObject().ShouldEqual(actual);
         }
 
         [TestMethod]
-        public void GetModString_Test()
+        public void FizzBuzz_stringBuilder_Test()
         {
-            SetUp();
-            Assert.AreEqual(_FizzBuzzC.GetModString(1), "1");
-            Assert.AreEqual(_FizzBuzzC.GetModString(3), "Fizz");
-            Assert.AreEqual(_FizzBuzzC.GetModString(5), "Buzz");
-            Assert.AreEqual(_FizzBuzzC.GetModString(15), "FizzBuzz");
+            var actual = _FizzBuzzService.FizzBuzz_stringBuilder(21, new[] { 3, 5, 7 });
+            _ExpectedList.ToExpectedObject().ShouldEqual(actual);
         }
+
+        [TestMethod]
+        public void FizzBuzz_Performance_Test()
+        {
+            Stopwatch watch1 = new Stopwatch();
+            watch1.Start();
+            Action act1 = () => _FizzBuzzService.FizzBuzz_string(90000000, new[] { 3, 5, 7, 11, 13, 17, 19, 23, 29 });
+            watch1.Stop();
+            Console.WriteLine("String       : {0}", watch1.Elapsed);
+
+            Stopwatch watch2 = new Stopwatch();
+            watch2.Start();
+            Action act2 = () => _FizzBuzzService.FizzBuzz_stringBuilder(90000000, new[] { 3, 5, 7, 11, 13, 17, 19, 23, 29 });
+            watch2.Stop();
+            Console.WriteLine("StringBuilder: {0}", watch2.Elapsed);
+        }
+
     }
 }
